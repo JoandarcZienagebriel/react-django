d101b77fe0eeb2a5ffec762f980d89a7b8d56d62
Joandarc Zienagebriel <joandarczienagebriel@gmail.com>
2026-09-10 23:49:27 +0300

    connect-to-neon-postgres-db

--- backend/django_admin/settings.py
+++ backend/django_admin/settings.py
@@ -81,12 +81,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'django_admin.wsgi.application'
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
+
 
 
 # Password validation
